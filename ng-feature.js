5261b558b1716ce85d1a59e286378bf4a721dfc5
Umut Topuzoğlu <uloco@schiefewelt.de>
2016-04-13 18:13:18 +0200

    Moved to Ecmascript 2015
    Enhanced features and moved syntax to es2015 style.
    TODO: add babel-polyfill and transpiler for dev environment

--- ng-feature.js
+++ ng-feature.js
@@ -1,98 +1,135 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('ng-feature', [])
-        .directive('ngFeature', ngFeature)
-        .controller('NgFeatureController', NgFeatureController)
-        .factory('$ngFeature', $ngFeature);
+  angular
+    .module('ng-feature', [])
+    .directive('ngFeature', ngFeature)
+    .factory('$ngFeature', $ngFeature);
+
+  /**
+   * Define feature element.
+   *
+   * @ngdoc directive
+   * @name ngFeature
+   * @param {string} name - Feature name of element
+   * @param {string[]} [features] - Optional list of available features.
+   */
+  function ngFeature($ngFeature, $log) {
+    return {
+      restrict: 'E',
+      scope: {
+        name: '@',
+        features: '=?'
+      },
+      transclude: true,
+      template: '<div ng-show="featureAvailable"><ng-transclude></ng-transclude></div>',
+      link: link
+    };
+
+    function link(scope) {
+      // get initial features
+      if (angular.isDefined(scope.features) && angular.isArray(scope.features) &&
+        scope.features.length > 0) {
+        scope.featureAvailable = $ngFeature.check(scope.name, scope.features);
+      } else if (angular.isUndefined(scope.features)) {
+        scope.featureAvailable = $ngFeature.check(scope.name);
+      }
+
+      // watch feature changes
+      scope.$watch('features', function (newValue) {
+        if (angular.isDefined(newValue) && angular.isArray(newValue) && newValue.length > 0) {
+          scope.featureAvailable = $ngFeature.check(scope.name, scope.features);
+        }
+      });
+
+      if (angular.isUndefined(scope.features)) {
+        scope.$watch($ngFeature.get, function (newValue, oldValue) {
+          if (newValue !== oldValue) {
+            scope.featureAvailable = $ngFeature.check(scope.name);
+            $log.warn(scope.featureAvailable);
+          }
+        }, true);
+      }
+    }
+  }
+
+  function $ngFeature($log) {
+    /**
+     * Enabled feature collection.
+     */
+    var list = new Set();
 
     /**
-     * Define feature element.
+     * Check if feature is available.
      *
-     * @ngdoc directive
-     * @name ngFeature
-     * @param {string} name - Feature name of element
-     * @param {string[]} [features] - Optional list of available features.
+     * @param {string} value - Feature name.
+     * @param {string[]} [lov] - List of available feature names. Will use global features list if
+     *   undefined.
+     * @returns {boolean}
      */
-    function ngFeature() {
-        return {
-            restrict: 'E',
-            scope: {
-                name: '=',
-                features: '=?'
-            },
-            transclude: true,
-            template: '<div ng-show="featureAvailable"><ng-transclude></ng-transclude></div>',
-            controller: 'NgFeatureController'
-        };
+    function check(value, lov) {
+      var features;
+
+      if (angular.isDefined(lov) && angular.isArray(lov) && lov.length > 0) {
+        features = new Set(lov);
+      } else if (angular.isDefined(value) && angular.isUndefined(lov)) {
+        features = list;
+      } else {
+        return false;
+      }
+      return features.has(value);
     }
 
-    function NgFeatureController($scope, $ngFeature) {
-        $scope.$watch('features', function (newValue) {
-            if (angular.isDefined(newValue) && angular.isArray(newValue) && newValue.length > 0) {
-                $scope.featureAvailable = $ngFeature._check($scope.name, $scope.features);
-            }
-        });
-
-        if (angular.isUndefined($scope.features)) {
-            $scope.$watch($ngFeature.getValues, function (newValue, oldValue) {
-                if (newValue !== oldValue) {
-                    $scope.featureAvailable = $ngFeature._check($scope.name, $ngFeature.getValues());
-                }
-            });
-        }
+    /**
+     * Get available features.
+     * @returns {String[]}
+     */
+    function get() {
+      return Array.from(list);
     }
 
-    function $ngFeature() {
-        /**
-         * @type {String[]} - Available global feature list.
-         */
-        var availableFeatures = [];
-
-        /**
-         * Check if feature is available.
-         *
-         * @param {string} value - Feature name.
-         * @param {string[]} [lov] - List of available feature names. Will use global features list if undefined.
-         * @returns {boolean}
-         */
-        function _check(value, lov) {
-            var features;
-
-            if (angular.isDefined(lov) && angular.isArray(lov) && lov.length > 0) {
-                features = lov;
-            } else if (angular.isDefined(value) && angular.isUndefined(lov)) {
-                features = availableFeatures;
-            } else {
-                return false;
-            }
-            return features.some(function (feature) {
-                return value === feature;
-            });
-        }
-
-        /**
-         * Get available features.
-         * @returns {String[]}
-         */
-        function getValues() {
-            return availableFeatures;
-        }
-
-        /**
-         * Set available features.
-         * @param {String[]} features
-         */
-        function setValues(features) {
-            availableFeatures = features;
-        }
-
-        return {
-            list: availableFeatures,
-            _check: _check,
-            getValues: getValues,
-            setValues: setValues
-        };
+    /**
+     * Set available features, overwrites existing ones.
+     * @param {...string} features
+     */
+    function set(...features) {
+      list = new Set(features);
+      $log.debug(`enabled features [${features}], overwriting existing ones.`);
     }
-}());
+
+    /**
+     * Adds features to the list.
+     * @param {...string} features
+     */
+    function add(...features) {
+      features.forEach(feature => list.add(feature));
+      $log.debug(`added features [${features}]`);
+    }
+
+    /**
+     * Removes given features from the list.
+     * @param {...string} features
+     */
+    function remove(...features) {
+      features.forEach(feature => list.delete(feature));
+      $log.debug(`removed features [${features}]`);
+    }
+
+    /**
+     * Clears the feature list.
+     */
+    function clear() {
+      list.clear();
+      $log.debug('cleared features list');
+    }
+
+    return {
+      check,
+      get,
+      set,
+      add,
+      remove,
+      clear
+    };
+  }
+})();
